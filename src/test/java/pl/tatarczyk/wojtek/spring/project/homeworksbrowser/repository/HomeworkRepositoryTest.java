84fa7357aa2a3b05e3fe5d8540fe7747acbd0a26
Wojtek Tatarczyk <wojtek.tatarczyk@wp.pl>
2022-05-30 17:58:21 +0200

    add new junit test for homework subject

--- src/test/java/pl/tatarczyk/wojtek/spring/project/homeworksbrowser/repository/HomeworkRepositoryTest.java
+++ src/test/java/pl/tatarczyk/wojtek/spring/project/homeworksbrowser/repository/HomeworkRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import pl.tatarczyk.wojtek.spring.project.homeworksbrowser.repository.entity.HomeworkEntity;
+import pl.tatarczyk.wojtek.spring.project.homeworksbrowser.repository.entity.SubjectEntity;
 
 @SpringBootTest
 class HomeworkRepositoryTest {
@@ -22,6 +23,19 @@ class HomeworkRepositoryTest {
         HomeworkEntity savedHomeworkEntity = homeworkRepository.save(homeworkEntity);
         //Then
         Assertions.assertNotNull(savedHomeworkEntity,"savedHomeworkEntity is null");
+
+    }
+
+    @Test
+    @Rollback(false)
+    void given_when_then1(){
+        //Given
+        HomeworkEntity homeworkEntity = new HomeworkEntity();
+        homeworkEntity.setSubject(SubjectEntity.FIZYKA);
+        //When
+        HomeworkEntity savedHomeworkEntity = homeworkRepository.save(homeworkEntity);
+        //Then
+        Assertions.assertEquals(SubjectEntity.FIZYKA, savedHomeworkEntity.getSubject(),"subject is not equals");
 
     }
 }
