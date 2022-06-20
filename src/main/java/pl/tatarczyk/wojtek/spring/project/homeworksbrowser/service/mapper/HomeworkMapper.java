71316177b6589d01f8a44942c896135d89a5a279
Wojtek Tatarczyk <wojtek.tatarczyk@wp.pl>
2022-06-20 17:40:22 +0200

    add mapper for homework from model to entity

--- src/main/java/pl/tatarczyk/wojtek/spring/project/homeworksbrowser/service/mapper/HomeworkMapper.java
+++ src/main/java/pl/tatarczyk/wojtek/spring/project/homeworksbrowser/service/mapper/HomeworkMapper.java
@@ -21,4 +21,22 @@ public class HomeworkMapper {
         LOGGER.info("from(...) = " + homeworkEntity);
         return homeworkEntity;
     }
+
+    public HomeworkModel from(HomeworkEntity homeworkEntity) {
+        LOGGER.info("from(" + homeworkEntity + ")");
+        HomeworkModel homeworkModel = new HomeworkModel();
+
+        homeworkModel.setId(homeworkEntity.getId());
+        homeworkModel.setTitle(homeworkEntity.getTitle());
+
+        LOGGER.info("from(...) = "+homeworkModel);
+
+        return homeworkModel;
+    }
+
 }
+
+
+// TODO: 13.06.2022 dopisać metode która zmienia z entity na model
+// dopisać metode która zamienia listę modeli na listę encji (użyć strumieni i lambd)
+// przepisać zmienne z entity do modeli
