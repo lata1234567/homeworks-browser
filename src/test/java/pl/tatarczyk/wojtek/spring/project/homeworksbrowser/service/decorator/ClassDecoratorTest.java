f9002336327991e0a88cee3ff28b1e285974688b
Wojtek Tatarczyk <wojtek.tatarczyk@wp.pl>
2022-11-07 17:48:39 +0100

    add Java unit test for ClassDecorator

--- src/test/java/pl/tatarczyk/wojtek/spring/project/homeworksbrowser/service/decorator/ClassDecoratorTest.java
+++ src/test/java/pl/tatarczyk/wojtek/spring/project/homeworksbrowser/service/decorator/ClassDecoratorTest.java
@@ -1,7 +1,29 @@
 package pl.tatarczyk.wojtek.spring.project.homeworksbrowser.service.decorator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pl.tatarczyk.wojtek.spring.project.homeworksbrowser.web.model.ClassModel;
+import pl.tatarczyk.wojtek.spring.project.homeworksbrowser.web.model.ClassNameModel;
 
 class ClassDecoratorTest {
-// TODO: 31.10.2022 napisać test jednoskowy dla metody decorate
+    private static final String CLASS_NAME = "1c";
+    private static final int YEAR = 1;
+
+    @Test
+    void decorate() {
+        //Given
+        ClassModel classModel = new ClassModel();
+        ClassNameModel classNameModel = new ClassNameModel();
+        classNameModel.setName(CLASS_NAME);
+        classModel.setClassName(classNameModel);
+        classModel.setYear(YEAR);
+        //When
+        String decoratedClass = ClassDecorator.decorate(classModel);
+        //Then
+        Assertions.assertAll(
+                () -> Assertions.assertNotNull(decoratedClass, "decoratedClass is null")
+        );
+
+    }
+
 }
